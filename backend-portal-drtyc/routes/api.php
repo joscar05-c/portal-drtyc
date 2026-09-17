c9e0b431dcaca2dbae535378af29b25dc72740ae
joscar05-c <pizangojosec@gmail.com>
2026-09-17 17:16:01 -0500

    api para consumir el rastreo de reclamos y quejas

--- backend-portal-drtyc/routes/api.php
+++ backend-portal-drtyc/routes/api.php
@@ -35,4 +35,6 @@ Route::prefix('v1')->group(function () {
 
     Route::post('/complaints', [ComplaintController::class, 'store']);
 
+    Route::post('/complaints/track', [ComplaintController::class, 'track']);
+
 });
