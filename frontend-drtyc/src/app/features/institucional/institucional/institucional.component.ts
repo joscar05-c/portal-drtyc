d22cbf8db252ab197408d0ffc641d54ca6df069b
joscar05-c <pizangojosec@gmail.com>
2026-09-18 17:33:28 -0500

    COMPONENTE PARA INFORMACION DE LA INSTITUCION

---
+++ frontend-drtyc/src/app/features/institucional/institucional/institucional.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-institucional',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './institucional.component.html',
+})
+export class InstitucionalComponent {}
