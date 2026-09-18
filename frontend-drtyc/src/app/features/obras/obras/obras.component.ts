a491549b69520905cf27d48a6bdb429103908a1b
joscar05-c <pizangojosec@gmail.com>
2026-09-18 17:34:02 -0500

    componente de obras

---
+++ frontend-drtyc/src/app/features/obras/obras/obras.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-obras',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './obras.component.html',
+})
+export class ObrasComponent {}
