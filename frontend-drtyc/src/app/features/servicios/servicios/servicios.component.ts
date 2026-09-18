dcaf39f0d0eb3b55c1cb3fbd15b0e5fcffb9e0df
joscar05-c <pizangojosec@gmail.com>
2026-09-18 17:34:21 -0500

    componente de servicios y trámites

---
+++ frontend-drtyc/src/app/features/servicios/servicios/servicios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-servicios',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './servicios.component.html',
+})
+export class ServiciosComponent {}
