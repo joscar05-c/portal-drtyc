d47143540ca4a805e23a583f102a2dcfb6346bac
joscar05-c <pizangojosec@gmail.com>
2026-09-21 13:00:29 -0500

    agregamos la relacion con el área al user

--- backend-portal-drtyc/app/Models/User.php
+++ backend-portal-drtyc/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Activitylog\Models\Concerns\LogsActivity;
@@ -38,6 +39,11 @@ class User extends Authenticatable
     public function documents(): HasMany
     {
         return $this->hasMany(Document::class);
+    }
+
+    public function area(): BelongsTo
+    {
+        return $this->belongsTo(Area::class);
     }
 
     public function getActivitylogOptions(): LogOptions
