6b3eb21380a6687c7c029bca9fff0795d852c497
joscar05-c <pizangojosec@gmail.com>
2026-09-22 18:12:41 -0500

    space

--- backend-portal-drtyc/app/Filament/Resources/UserResource.php
+++ backend-portal-drtyc/app/Filament/Resources/UserResource.php
@@ -37,6 +37,7 @@ class UserResource extends Resource
 
     public static function canAccess(): bool
     {
+
         return auth()->user()->hasAnyRole(['super_admin', 'admin', 'editor']);
     }
 
